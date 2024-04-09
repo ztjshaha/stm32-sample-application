44c8e88e69c941bdb02b7959f1eb22464b30c33d
PaddyZhang <76602430@qq.com>
2024-04-09 16:19:07 +0800

    update cmocka to ci

--- applications/sample_application/targets/template/STM32F4/Core/Src/freertos.c
+++ applications/sample_application/targets/template/STM32F4/Core/Src/freertos.c
@@ -25,7 +25,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "crc.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
