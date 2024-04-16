23850407b9e6fab9a52ac2fa6a8a6172b6f13b63
ztjshaha <ztjshaha@gmail.com>
2024-04-16 23:32:56 +0800

    update device to functions

--- applications/sample_application/sdk/include/emmV5.h
+++ applications/sample_application/sdk/include/emmV5.h
@@ -26,21 +26,70 @@ typedef enum {
 	S_ORG   = 16,
 }SysParams_t;
 
-sdkErr_t emmV5EnControl(uint8_t addr, bool state, bool snF);
-sdkErr_t emmV5ModifyCtrlMode(uint8_t addr, bool svF, uint8_t ctrl_mode);
-sdkErr_t emmV5PosControl(uint32_t addr, uint8_t dir, uint16_t vel, uint8_t acc, uint32_t clk, bool raF, bool snF);
-sdkErr_t emmV5readMotorTargetPosition(uint32_t addr);
-sdkErr_t emmV5readMotorSpeed(uint32_t addr);
-sdkErr_t emmV5ResetCurPosToZero(uint32_t addr);
-sdkErr_t emmV5ResetClogPro(uint32_t addr);
-sdkErr_t emmV5VelControl(uint32_t addr, uint8_t dir, uint16_t vel, uint8_t acc, bool snF);
-sdkErr_t emmV5PosControl(uint32_t addr, uint8_t dir, uint16_t vel, uint8_t acc, uint32_t clk, bool raF, bool snF);
-sdkErr_t emmV5ReadSysParams(uint32_t addr, SysParams_t s);
-sdkErr_t emmV5StopNow(uint32_t addr, bool snF);
-sdkErr_t emmV5Synchronousmotion(uint8_t addr);
-sdkErr_t emmV5OriginModifyParams(uint8_t addr, bool svF, uint8_t o_mode, uint8_t o_dir, uint16_t o_vel, uint32_t o_tm, uint16_t sl_vel, uint16_t sl_ma, uint16_t sl_ms, bool potF);
-sdkErr_t emmV5OriginTriggerReturn(uint8_t addr, uint8_t o_mode, bool snF);
-sdkErr_t emmV5OriginInterrupt(uint8_t addr);
+typedef enum {
+	ONELOOP   = 0,
+	ONELOOPDIR    = 1,
+	MULLOOPNOLIMIT   = 2,
+	MULLOOPLIMIT  = 3
+}ZeroMode_t;
+
+//define emmV5 Funtion mask
+#define smEnControl 		0xF3
+#define smSpeedModeControl 	0xF6
+#define smPositionModeControl 	0xFD
+#define smStopNowControl 		0xFE
+#define smSyncMotion 			0xFF
+
+#define smOneLoopZeroPos 		0x93
+#define smZeroPos 				0x9A
+#define smForceDisableReturnZeo 0x9C
+#define smReadReturnZeroParam 	0x22
+#define smChangeReturnZeroParam 0x4C
+#define smReadReturnZeroFlag 	0x3B
+
+#define smTrigeEncoderCal 		0x06
+#define smClearCurPosRot 		0x0A
+#define smClearStallProtect 	0x0E
+#define smFactoryReset 			0x0F
+
+#define smReadFWHWVersion 		0x1F
+#define smReadResPhaseHenrys 	0x20
+#define smReadPosLoopPIDParam 	0x21
+#define smReadBusVoltage 		0x24
+#define smReadPhaseCurrent 		0x27
+#define smReadPostCalEncoderValue 	0x31
+#define smReadInPluse 				0x32
+#define smReadTargetPos 			0x33
+#define smReadSetTargetPos 			0x34
+#define smReadCurSpeed 				0x35
+#define smReadCurPos 				0x36
+#define smReadPosErr 				0x37
+#define smReadStatusFlag 			0x3A
+#define smReadDriverConfigParams 	0x42
+#define smReadSysStatusParams 		0x43
+
+#define smChangeResolution 			0x84
+#define smChangeIdAddress 			0xAE
+#define smChangeCtrlMode 			0x46
+#define smChangeOpenModeCurrent 	0x44
+#define smChangeDriverConfigParams 	0x48
+#define smChangePosLoopPIDParams 	0x4A
+#define smChangeComModeSpeed 		0x4F
+
+sdkErr_t emmV5EnControl(stepmotor_device_t *device, stepmotor_status_t state, bool snF);
+sdkErr_t emmV5ModifyCtrlMode(stepmotor_device_t *device, bool snF, uint8_t ctrl_mode);
+sdkErr_t emmV5PosControl(stepmotor_device_t *device,uint8_t dir, uint16_t vel, uint8_t acc, uint32_t clk, bool raF, bool snF);
+sdkErr_t emmV5readMotorTargetPosition(stepmotor_device_t *device);
+sdkErr_t emmV5readMotorSpeed(stepmotor_device_t *device);
+sdkErr_t emmV5ResetCurPosToZero(stepmotor_device_t *device);
+sdkErr_t emmV5ClearStallPro(stepmotor_device_t *device);
+sdkErr_t emmV5VelControl(stepmotor_device_t *device, uint8_t dir, uint16_t vel, uint8_t acc, bool snF);
+sdkErr_t emmV5ReadSysParams(stepmotor_device_t *device, SysParams_t s);
+sdkErr_t emmV5StopNow(stepmotor_device_t *device, bool snF);
+sdkErr_t emmV5Synchronousmotion(stepmotor_device_t *device);
+sdkErr_t emmV5OriginModifyParams(stepmotor_device_t *device, bool svF, uint8_t o_mode, uint8_t o_dir, uint16_t o_vel, uint32_t o_tm, uint16_t sl_vel, uint16_t sl_ma, uint16_t sl_ms, bool potF);
+sdkErr_t emmV5OriginTriggerReturn(stepmotor_device_t *device, ZeroMode_t o_mode, bool snF);
+sdkErr_t emmV5OriginInterrupt(stepmotor_device_t *device);
 
 #ifdef __cplusplus
 }
--- applications/sample_application/sdk/src/emmV5.c
+++ applications/sample_application/sdk/src/emmV5.c
@@ -1,44 +1,50 @@
 #include "../include/emmV5.h"
 
-sdkErr_t emmV5EnControl(uint8_t addr, bool state, bool snF)
+sdkErr_t emmV5EnControl(stepmotor_device_t *device,stepmotor_status_t state, bool snF)
 {
   uint8_t buffer[16] = {0};
-
-  buffer[0] =  0xF3;
+  device->status = state;
+  device->snF = snF;
+  buffer[0] =  smEnControl;
   buffer[1] =  0xAB;
-  buffer[2] =  (uint8_t)state;
-  buffer[3] =  snF;
+  buffer[2] =  device->status;
+  buffer[3] =  device->snF;
   buffer[4] =  0x6B;
 
-	if( can_send_msg(addr,buffer,5) != SDK_OK)
-	{
-		return SDK_ERR;
-	}
+  if( can_send_msg(device->device_id,buffer,5) != SDK_OK)
+  {
+	return SDK_ERR;
+  }
 
-	return SDK_OK;
+  return SDK_OK;
 }
 
-sdkErr_t emmV5ModifyCtrlMode(uint8_t addr, bool svF, uint8_t ctrl_mode)
+sdkErr_t emmV5ModifyCtrlMode(stepmotor_device_t *device, bool snF, uint8_t ctrl_mode)
 {
   uint8_t buffer[16] = {0};
 
-  buffer[0] =  0x46;
+  device->snF = snF;
+  device->ctrl_mode = ctrl_mode;
+  buffer[0] =  smChangeCtrlMode;
   buffer[1] =  0x69;
-  buffer[2] =  svF;
-  buffer[3] =  ctrl_mode;
+  buffer[2] =  device->snF;
+  buffer[3] =  device->ctrl_mode;
   buffer[4] =  0x6B;
 
-  if( can_send_msg(addr,buffer,5) != SDK_OK)
+  if( can_send_msg(device->device_id,buffer,5) != SDK_OK)
   {
 	  return SDK_ERR;
   }
   return SDK_OK;
 }
 
-sdkErr_t emmV5readMotorTargetPosition(uint32_t addr)
+sdkErr_t emmV5readMotorTargetPosition(stepmotor_device_t *device)
 {
-	uint8_t buffer[2] = {0x34,0x6B};
-	if( can_send_msg(addr,buffer,2) != SDK_OK)
+	uint8_t buffer[16] = {0};
+
+	buffer[0] = smReadSetTargetPos;
+	buffer[1] = 0x6B;
+	if( can_send_msg(device->device_id,buffer,2) != SDK_OK)
 	{
 		return SDK_ERR;
 	}
@@ -46,10 +52,13 @@ sdkErr_t emmV5readMotorTargetPosition(uint32_t addr)
 	return SDK_OK;
 }
 
-sdkErr_t emmV5readMotorSpeed(uint32_t addr)
+sdkErr_t emmV5readMotorSpeed(stepmotor_device_t *device)
 {
-	uint8_t buffer[2] = {0x35,0x6B};
-	if( can_send_msg(addr,buffer,2) != SDK_OK)
+	uint8_t buffer[16] = {0};
+
+	buffer[0] = smReadCurSpeed;
+	buffer[1] = 0x6B;
+	if( can_send_msg(device->device_id,buffer,2) != SDK_OK)
 	{
 		return SDK_ERR;
 	}
@@ -57,15 +66,15 @@ sdkErr_t emmV5readMotorSpeed(uint32_t addr)
 	return SDK_OK;
 }
 
-sdkErr_t emmV5ResetCurPosToZero(uint32_t addr)
+sdkErr_t emmV5ResetCurPosToZero(stepmotor_device_t *device)
 {
     uint8_t buffer[16] = {0x00};
 
-    buffer[0] =  0x0A;
+    buffer[0] =  smClearCurPosRot;
     buffer[1] =  0x6D;
     buffer[2] =  0x6B;
 
-	if( can_send_msg(addr,buffer,3) != SDK_OK)
+	if( can_send_msg(device->device_id,buffer,3) != SDK_OK)
 	{
 		return SDK_ERR;
 	}
@@ -73,15 +82,15 @@ sdkErr_t emmV5ResetCurPosToZero(uint32_t addr)
 	return SDK_OK;
 }
 
-sdkErr_t emmV5ResetClogPro(uint32_t addr)
+sdkErr_t emmV5ClearStallPro(stepmotor_device_t *device)
 {
     uint8_t buffer[16] = {0x00};
 
-    buffer[0] =  0x0E;
+    buffer[0] =  smClearStallProtect;
     buffer[1] =  0x52;
     buffer[2] =  0x6B;
 
-	if( can_send_msg(addr,buffer,3) != SDK_OK)
+	if( can_send_msg(device->device_id,buffer,3) != SDK_OK)
 	{
 		return SDK_ERR;
 	}
@@ -89,152 +98,150 @@ sdkErr_t emmV5ResetClogPro(uint32_t addr)
 	return SDK_OK;
 }
 
-sdkErr_t emmV5VelControl(uint32_t addr, uint8_t dir, uint16_t vel, uint8_t acc, bool snF)
+sdkErr_t emmV5VelControl(stepmotor_device_t *device, uint8_t dir, uint16_t vel, uint8_t acc, bool snF)
 {
     uint8_t buffer[16] = {0x00};
-    uint8_t check=0;
-    buffer[0] =  0xF6;
-    buffer[1] =  dir;
-    buffer[2] =  (uint8_t)(vel >> 8);
-    buffer[3] =  (uint8_t)(vel >> 0);
-    buffer[4] =  acc;
-    buffer[5] =  snF;
+
+    device->dir = dir;
+    device->speed = vel;
+    device->acceleration = acc;
+    device->snF = snF;
+
+    buffer[0] =  smSpeedModeControl;
+    buffer[1] =  device->dir;
+    buffer[2] =  (uint8_t)(device->speed >> 8);
+    buffer[3] =  (uint8_t)(device->speed >> 0);
+    buffer[4] =  device->acceleration;
+    buffer[5] =  device->snF;
     buffer[6] =  0x6B;
 
-	if( can_send_msg(addr,buffer,7) != SDK_OK)
+	if( can_send_msg(device->device_id,buffer,7) != SDK_OK)
 	{
 		return SDK_ERR;
 	}
-
-	while(can.rxFrameFlag == false);
-	for(int i = 0;i < can.CAN_RxMsg.DLC; i++)
-	{
-	     if((RxData[0] == 0xF7)||(RxData[1] == 0x02)||(RxData[2] == 0x6b))
-	    	 check = 1;
-	     else
-	    	 check = 0;
-	}
-	can.rxFrameFlag = false;
-	if(check == 0)
-		return SDK_ERR;
-	else
 	return SDK_OK;
 }
-sdkErr_t emmV5PosControl(uint32_t addr, uint8_t dir, uint16_t vel, uint8_t acc, uint32_t clk, bool raF, bool snF)
+
+sdkErr_t emmV5PosControl(stepmotor_device_t *device,uint8_t dir, uint16_t vel, uint8_t acc, uint32_t clk, bool raF, bool snF)
 {
+  device->dir = dir;
+  device->speed = vel;
+  device->acceleration = acc;
+  device->pluse = clk;
+  device->raF = raF;
+  device->snF = snF;
   uint8_t buffer0[8] = {0x00};
   uint8_t buffer1[8] = {0x00};
   buffer0[0]  =  0xFD;                      // 功能码
-  buffer0[1]  =  dir;                       // 方向
-  buffer0[2]  =  (uint8_t)(vel >> 8);       // 速度(RPM)高8位字节
-  buffer0[3]  =  (uint8_t)(vel >> 0);       // 速度(RPM)低8位字节
-  buffer0[4]  =  acc;                       // 加速度，注意：0是直接启动
-  buffer0[5]  =  (uint8_t)(clk >> 24);      // 脉冲数(bit24 - bit31)
-  buffer0[6]  =  (uint8_t)(clk >> 16);      // 脉冲数(bit16 - bit23)
-  buffer0[7] =  (uint8_t)(clk >> 8);       // 脉冲数(bit8  - bit15)
+  buffer0[1]  =  device->dir;                       // 方向
+  buffer0[2]  =  (uint8_t)(device->speed >> 8);       // 速度(RPM)高8位字节
+  buffer0[3]  =  (uint8_t)(device->speed >> 0);       // 速度(RPM)低8位字节
+  buffer0[4]  =  device->acceleration;                       // 加速度，注意：0是直接启动
+  buffer0[5]  =  (uint8_t)(device->pluse >> 24);      // 脉冲数(bit24 - bit31)
+  buffer0[6]  =  (uint8_t)(device->pluse >> 16);      // 脉冲数(bit16 - bit23)
+  buffer0[7] =  (uint8_t)(device->pluse >> 8);       // 脉冲数(bit8  - bit15)
 
   buffer1[0]  =  0xFD;                      // 功能码
-  buffer1[1] =  (uint8_t)(clk >> 0);       // 脉冲数(bit0  - bit7 )
-  buffer1[2] =  raF;                       // 相位/绝对标志，false为相对运动，true为绝对值运动
-  buffer1[3] =  snF;                       // 多机同步运动标志，false为不启用，true为启用
+  buffer1[1] =  (uint8_t)(device->pluse >> 0);       // 脉冲数(bit0  - bit7 )
+  buffer1[2] =  device->raF;                       // 相位/绝对标志，false为相对运动，true为绝对值运动
+  buffer1[3] =  device->snF;                       // 多机同步运动标志，false为不启用，true为启用
   buffer1[4] =  0x6B;                      // 校验字节
-
-  if( can_send_msg(addr,buffer0,8) != SDK_OK)
+  if( can_send_msg(device->device_id,buffer0,8) != SDK_OK)
   {
       return SDK_ERR;
   }
-  if( can_send_msg(addr+1,buffer1,5) != SDK_OK)
+  if( can_send_msg(device->device_id + 1,buffer1,5) != SDK_OK)
   {
       return SDK_ERR;
   }
   return SDK_OK;
 }
 
-sdkErr_t emmV5ReadSysParams(uint32_t addr, SysParams_t s)
+sdkErr_t emmV5ReadSysParams(stepmotor_device_t *device, SysParams_t s)
 {
   uint8_t i = 0;
   uint8_t buffer[16] = {0x00};
 
   switch(s)
   {
-    case S_VER  : buffer[i] = 0x1F; ++i; break;
-    case S_RL   : buffer[i] = 0x20; ++i; break;
-    case S_PID  : buffer[i] = 0x21; ++i; break;
-    case S_VBUS : buffer[i] = 0x24; ++i; break;
-    case S_CPHA : buffer[i] = 0x27; ++i; break;
-    case S_ENCL : buffer[i] = 0x31; ++i; break;
-    case S_TPOS : buffer[i] = 0x33; ++i; break;
-    case S_VEL  : buffer[i] = 0x35; ++i; break;
-    case S_CPOS : buffer[i] = 0x36; ++i; break;
-    case S_PERR : buffer[i] = 0x37; ++i; break;
-    case S_FLAG : buffer[i] = 0x3A; ++i; break;
-    case S_ORG  : buffer[i] = 0x3B; ++i; break;
-    case S_Conf : buffer[i] = 0x42; ++i; buffer[i] = 0x6C; ++i; break;
-    case S_State: buffer[i] = 0x43; ++i; buffer[i] = 0x7A; ++i; break;
+    case S_VER  : buffer[i] = smReadFWHWVersion; ++i; break;
+    case S_RL   : buffer[i] = smReadResPhaseHenrys; ++i; break;
+    case S_PID  : buffer[i] = smReadPosLoopPIDParam; ++i; break;
+    case S_VBUS : buffer[i] = smReadBusVoltage; ++i; break;
+    case S_CPHA : buffer[i] = smReadPhaseCurrent; ++i; break;
+    case S_ENCL : buffer[i] = smReadPostCalEncoderValue; ++i; break;
+    case S_TPOS : buffer[i] = smReadTargetPos; ++i; break;
+    case S_VEL  : buffer[i] = smReadCurSpeed; ++i; break;
+    case S_CPOS : buffer[i] = smReadCurPos; ++i; break;
+    case S_PERR : buffer[i] = smReadPosErr; ++i; break;
+    case S_FLAG : buffer[i] = smReadStatusFlag; ++i; break;
+    case S_ORG  : buffer[i] = smReadReturnZeroFlag; ++i; break;
+    case S_Conf : buffer[i] = smReadDriverConfigParams; ++i; buffer[i] = 0x6C; ++i; break;
+    case S_State: buffer[i] = smReadSysStatusParams; ++i; buffer[i] = 0x7A; ++i; break;
     default: break;
   }
 
   buffer[i] = 0x6B; ++i;
 
-  if( can_send_msg(addr,buffer,i) != SDK_OK)
+  if( can_send_msg(device->device_id,buffer,i) != SDK_OK)
   {
 	  return SDK_ERR;
   }
   return SDK_OK;
 }
 
-sdkErr_t emmV5StopNow(uint32_t addr, bool snF)
+sdkErr_t emmV5StopNow(stepmotor_device_t *device, bool snF)
 {
     uint8_t buffer[16] = {0x00};
-
-    buffer[0] =  0xFE;
+    device->snF = snF;
+    buffer[0] =  smStopNowControl;
     buffer[1] =  0x98;
-    buffer[2] =  snF;
+    buffer[2] =  device->snF;
     buffer[3] =  0x6B;
 
-    if( can_send_msg(addr,buffer,4) != SDK_OK)
+    if( can_send_msg(device->device_id,buffer,4) != SDK_OK)
     {
   	  return SDK_ERR;
     }
     return SDK_OK;
 }
-sdkErr_t emmV5Synchronousmotion(uint8_t addr)
+sdkErr_t emmV5Synchronousmotion(stepmotor_device_t *device)
 {
   uint8_t buffer[16] = {0};
 
-  buffer[0] =  0xFF;
+  buffer[0] =  smSyncMotion;
   buffer[1] =  0x66;
   buffer[2] =  0x6B;
 
-  if( can_send_msg(addr,buffer,3) != SDK_OK)
+  if( can_send_msg(device->device_id,buffer,3) != SDK_OK)
   {
 	  return SDK_ERR;
   }
   return SDK_OK;
 }
 
-sdkErr_t emmV5OriginSetO(uint8_t addr, bool svF)
+sdkErr_t emmV5OriginSetO(stepmotor_device_t *device, bool svF)
 {
   uint8_t buffer[16] = {0};
 
-  buffer[0] =  0x93;
+  buffer[0] =  smOneLoopZeroPos;
   buffer[1] =  0x88;
   buffer[2] =  svF;
   buffer[3] =  0x6B;
 
-  if( can_send_msg(addr,buffer,4) != SDK_OK)
+  if( can_send_msg(device->device_id,buffer,4) != SDK_OK)
   {
 	  return SDK_ERR;
   }
   return SDK_OK;
 }
 
-sdkErr_t emmV5OriginModifyParams(uint8_t addr, bool svF, uint8_t o_mode, uint8_t o_dir, uint16_t o_vel, uint32_t o_tm, uint16_t sl_vel, uint16_t sl_ma, uint16_t sl_ms, bool potF)
+sdkErr_t emmV5OriginModifyParams(stepmotor_device_t *device, bool svF, uint8_t o_mode, uint8_t o_dir, uint16_t o_vel, uint32_t o_tm, uint16_t sl_vel, uint16_t sl_ma, uint16_t sl_ms, bool potF)
 {
   uint8_t buffer[32] = {0};
 
 
-  buffer[0] =  0x4C;
+  buffer[0] =  smChangeReturnZeroParam;
   buffer[1] =  0xAE;
   buffer[2] =  svF;
   buffer[3] =  o_mode;
@@ -242,59 +249,60 @@ sdkErr_t emmV5OriginModifyParams(uint8_t addr, bool svF, uint8_t o_mode, uint8_t
   buffer[5]  =  (uint8_t)(o_vel >> 8);
   buffer[6]  =  (uint8_t)(o_vel >> 0);
   buffer[7]  =  (uint8_t)(o_tm >> 24);
-  if( can_send_msg(addr,buffer,8) != SDK_OK)
+  if( can_send_msg(device->device_id,buffer,8) != SDK_OK)
   {
 	  return SDK_ERR;
   }
 
-  buffer[0]  =  (uint8_t)(o_tm >> 16);
-  buffer[1] =  (uint8_t)(o_tm >> 8);
-  buffer[2] =  (uint8_t)(o_tm >> 0);
-  buffer[3] =  (uint8_t)(sl_vel >> 8);
-  buffer[4] =  (uint8_t)(sl_vel >> 0);
-  buffer[5] =  (uint8_t)(sl_ma >> 8);
-  buffer[6] =  (uint8_t)(sl_ma >> 0);
-  buffer[7] =  (uint8_t)(sl_ms >> 8);
-  if( can_send_msg(addr + 1,buffer,8) != SDK_OK)
+  buffer[1]  =  (uint8_t)(o_tm >> 16);
+  buffer[2] =  (uint8_t)(o_tm >> 8);
+  buffer[3] =  (uint8_t)(o_tm >> 0);
+  buffer[4] =  (uint8_t)(sl_vel >> 8);
+  buffer[5] =  (uint8_t)(sl_vel >> 0);
+  buffer[6] =  (uint8_t)(sl_ma >> 8);
+  buffer[7] =  (uint8_t)(sl_ma >> 0);
+  if( can_send_msg(device->device_id + 1,buffer,8) != SDK_OK)
   {
 	  return SDK_ERR;
   }
 
-  buffer[0] =  (uint8_t)(sl_ms >> 0);
-  buffer[1] =  potF;
-  buffer[2] =  0x6B;
-  if( can_send_msg(addr + 2,buffer,3) != SDK_OK)
+  buffer[1] =  (uint8_t)(sl_ms >> 8);
+  buffer[2] =  (uint8_t)(sl_ms >> 0);
+  buffer[3] =  potF;
+  buffer[4] =  0x6B;
+  if( can_send_msg(device->device_id + 2,buffer,4) != SDK_OK)
   {
 	  return SDK_ERR;
   }
   return SDK_OK;
 }
 
-sdkErr_t emmV5OriginTriggerReturn(uint8_t addr, uint8_t o_mode, bool snF)
+sdkErr_t emmV5OriginTriggerReturn(stepmotor_device_t *device, ZeroMode_t o_mode, bool snF)
 {
   uint8_t buffer[16] = {0};
 
-  buffer[0] =  0x9A;
+  device->snF = snF;
+  buffer[0] =  smZeroPos;
   buffer[1] =  o_mode;
   buffer[2] =  snF;
   buffer[3] =  0x6B;
 
-  if( can_send_msg(addr,buffer,4) != SDK_OK)
+  if( can_send_msg(device->device_id,buffer,4) != SDK_OK)
   {
 	  return SDK_ERR;
   }
   return SDK_OK;
 }
 
-sdkErr_t emmV5OriginInterrupt(uint8_t addr)
+sdkErr_t emmV5OriginInterrupt(stepmotor_device_t *device)
 {
   uint8_t buffer[16] = {0};
 
-  buffer[0] =  0x9C;
+  buffer[0] =  smForceDisableReturnZeo;
   buffer[1] =  0x48;
   buffer[2] =  0x6B;
 
-  if( can_send_msg(addr,buffer,3) != SDK_OK)
+  if( can_send_msg(device->device_id,buffer,3) != SDK_OK)
   {
 	  return SDK_ERR;
   }
